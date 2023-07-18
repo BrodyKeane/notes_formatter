4f5a9efcab12ee385d50a3776650f3b626e1792d
brody <keanebrody8585@gmail.com>
2023-07-18 01:31:19 -0400

    formatter complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::{fs, env};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let path = match args.get(1) {
+        Some(path) => path,
+        None => panic!("Expected file path after 'format'"),
+    };
+    
+    let input = fs::read_to_string(path)
+        .unwrap_or_else(|path| panic!("Unable to read file: {}", path));
+    
+    let output = format_string(input);
+
+    fs::write(path, output)
+        .unwrap_or_else(|path| panic!("Unable to write to file: {}", path));
+}
+
+fn format_string(input: String) -> String {
+    let mut formatted_string = String::new();
+    
+    for mut line in input.lines() {
+        if line.is_empty() {continue}
+        line = line.trim_start_matches(|c: char| !c.is_alphabetic());
+        line = line.trim_end_matches(|c: char| !c.is_alphabetic());
+        let formatted_line = &format!("### {}\n\n\n\n", &line);
+        formatted_string.push_str(formatted_line)
+    }
+
+    formatted_string
 }
---
+++ test.md
@@ -0,0 +1,64 @@
+### Identify the transmission control protocol/internet protocol (TCP/IP) model
+
+
+
+### Identify the Open Systems Interconnection (OSI) model
+
+
+
+### Describe the relationship between the OSI layers and the TCP/IP layers
+
+
+
+### Describe the data flow in the OSI model
+
+
+
+### Identify the types of basic network media
+
+
+
+### Describe the types of wired and wireless devices for networking
+
+
+
+### Explain distance limits of wireless media
+
+
+
+### Identify common networking commands
+
+
+
+### Identify various network types
+
+
+
+### Describe various network topologies
+
+
+
+### Describe diagrams of various network topologies
+
+
+
+### Describe various types of network architecture
+
+
+
+### Explain the differences between peer-to-peer and client/server architecture
+
+
+
+### Describe the fundamentals of cloud computing
+
+
+
+### Explain the role of virtualization in cloud computing
+
+
+
+### Describe how cloud computing supports high availability
+
+
+
